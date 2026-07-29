4364293ad0fcef4eccc5f1db1dcc1b93e98db6a6
Bia <biagomes96100@gmail.com>
2026-07-29 15:54:45 -0300

    Interface para garantir os métodos de salvar e carregar os dados

--- src/persistence/Persistivel.java
+++ src/persistence/Persistivel.java
@@ -1,4 +1,11 @@
 package persistence;
 
-public interface Persistivel {
+import java.util.List;
+import exception.ArquivoException;
+
+// interface para garantir os métodos de persistencia dos arquivos nos repositorios
+
+public interface Persistivel<T> {
+    void salvar(List<T> itens) throws ArquivoException;
+    List<T> carregar() throws ArquivoException;
 }
