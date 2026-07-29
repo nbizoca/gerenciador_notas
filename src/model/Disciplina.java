49a9ad249b697055b56c05aa798ac0ec6f461830
lucas310fernando-eng <lucas310fernando@gmail.com>
2026-07-29 14:53:19 -0300

    Adicionando os Métodos principais para a Classe Disciplina

--- src/model/Disciplina.java
+++ src/model/Disciplina.java
@@ -1,4 +1,50 @@
 package model;
 
 public class Disciplina {
+
+    private String codigo;
+    private String nome;
+    private int cargaHoraria;
+
+    public Disciplina(String codigo, String nome, int cargaHoraria) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    // para criar disciplina sem dizer a carga horária (fica 60h)
+    public Disciplina(String codigo, String nome) {
+        this(codigo, nome, 60);
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s (%dh)", codigo, nome, cargaHoraria);
+    }
+
+
 }
