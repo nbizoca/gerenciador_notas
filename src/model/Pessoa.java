6ec42784ee312cdc10b5ed24b0d9a7b046c9564b
Bia <biagomes96100@gmail.com>
2026-07-29 14:29:20 -0300

    Adicionando código da classe Pessoa com atributos e métodos

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -1,4 +1,45 @@
 package model;
 
-public class Pessoa {
+/**
+ * classe abstrata que representa uma pessoa genérica
+ */
+public abstract class Pessoa {
+
+    protected String nome;
+    protected String cpf;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome == null || nome.trim().isEmpty()) {
+            throw new IllegalArgumentException("Nome não pode ser vazio.");
+        }
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        if (cpf == null || cpf.trim().isEmpty()) {
+            throw new IllegalArgumentException("Cpf não pode ser vazio.");
+        }
+        this.cpf = cpf;
+    }
+
+    /** método abstrato para exibir os dados */
+    public abstract String exibirDados();
+
+    @Override
+    public String toString() {
+        return exibirDados();
+    }
 }
